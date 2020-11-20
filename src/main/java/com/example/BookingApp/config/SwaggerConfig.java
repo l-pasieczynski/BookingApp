b89b2e64296e02e65f5200285d8700a60db8fb21
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-11-20 19:58:36 +0100

    change package name from Config to config

--- src/main/java/com/example/BookingApp/config/SwaggerConfig.java
+++ src/main/java/com/example/BookingApp/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.BookingApp.Config;
+package com.example.BookingApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
